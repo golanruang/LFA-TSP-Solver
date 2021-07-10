66578fff4041fcd8c397717907bda5c866b98b2f
golanruang <guang.rolan@gmail.com>
2021-07-10 00:44:35 -0700

    able to sample tours

--- tsp.py
+++ tsp.py
@@ -30,7 +30,7 @@ class TSP:
         self.sample = [0,1,2,4,3]
         self.cities = cities
         self.numCities = 7
-        self.currCity = 2
+        self.currCity = "0"
         # self.history = [1,3,6]
         pass 
 
@@ -39,6 +39,9 @@ class TSP:
     
     def sampleTour(self):
         pass
+
+    def getNextCity(self, history):
+        return self.sampleNextCity(self.softmaxCities(self.phi(history)),history)
 
     def softmaxCities(self, phi):
         """
@@ -65,13 +68,12 @@ class TSP:
             softmaxOnly.append(city[-1])
         s = np.random.choice(softmaxOnly)
         index = -1
-        print("softmax only: ", softmaxOnly)
+        # ("softmax only: ", softmaxOnly)
         for i in range(len(softmaxCities)): # don't use this list
             if softmaxCities[i][-1]==s:
                 index=i
 
         nextCity = softmaxCities[index][2]
-        history.append(self.currCity)
         
         return nextCity
 
@@ -81,9 +83,10 @@ class TSP:
         """
         availableCities = []
         for city in self.cities.keys():
-            if int(city) not in history and int(city) != self.currCity:
+            if city not in history and city != self.currCity:
                 availableCities.append(city)
-        print(availableCities)
+        # print("history: ", history)
+        # print("available cities: ", availableCities)
         return availableCities
 
     def phi(self, history):
@@ -98,30 +101,17 @@ class TSP:
         for i in range(len(availableCities)):
             currentCity = self.cities[str(self.currCity)]
             nextCity = self.cities[str(availableCities[i])]
-            prevCity = self.cities[str(history[-1])]
+            if len(history)==0:
+                prevCity = currentCity
+            else: 
+                prevCity = self.cities[str(history[-1])]
             dist1 = self.distance(currentCity, prevCity)
             dist2 = self.distance(currentCity, nextCity)
-            phi.append([dist1+dist2, self.currCity, availableCities[i], history[-1]])
-        # for i in range(len(availableCities)):
-        #     for j in range(len(availableCities)):
-        #         print("availableCities[i]: ", availableCities[i])
-        #         currCity = self.cities[str(availableCities[i])]
-        #         print("currCity: ", currCity)
-        #         iterCity = self.cities[str(availableCities[j])]
-        #         print("iterCity: ", iterCity)
-        #         dist1 = self.distance(currCity, iterCity)
-        #         dist2 = self.distance(currCity, nextCity)
-        #         print("dist1: ", dist1)
-        #         print("dist2: ", dist2)
-        #         phi.append((dist1 + dist2,j))
-        #     phis.append((phi,currCity))
-        #     phi=[]
-        # print(phis)
-        print("phi in phi(): ", phi)
-
-        print("softmaxed: ", self.softmaxCities(phi))
-        nextCity = self.sampleNextCity(self.softmaxCities(phi), history)
-        print("next: ", nextCity)
+            if len(history)==0:
+                phi.append([dist1+dist2, self.currCity,
+                            availableCities[i], 0])
+            else: 
+                phi.append([dist1+dist2, self.currCity, availableCities[i], history[-1]])
         return phi
 
     def findPermutations(self):
@@ -135,8 +125,13 @@ def plot(path, cities):
     path = [0,5,4,3,2,6,0]
     
 def main():
-    numCities = 5
+    numCities = 7
     cities = {"0":(0.2,0.2), "1":(0.7,0.4), "2":(0,1,0.6), "3":(0.9,0.9), "4": (0.5,0.4), "5": (0.4,0.3), "6": (0.3,0.2)}
+    history = ["0"]
     t = TSP(5, cities) # num cities, stepsize, 
-    t.phi([1,3])
+    for i in range(numCities-1):
+        # t.getNextCity([1,3])
+        history.append(t.getNextCity(history))
+    history.append("0")
+    print(history)
 main()
